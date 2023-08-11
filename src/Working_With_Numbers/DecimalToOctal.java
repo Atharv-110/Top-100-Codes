6b5bf9765d7310669ffbc4c43bf67b9123067671
Atharv Vani <atharvavani110@gmail.com>
2023-08-12 03:20:32 +0530

    Working with numbers>DecimalToOctal>updated

---
+++ src/Working_With_Numbers/DecimalToOctal.java
@@ -0,0 +1,20 @@
+// Write a program to convert Decimal number to Octal Number System
+
+package Working_With_Numbers;
+
+public class DecimalToOctal {
+    public static void main(String[] args) {
+        int number = 148;
+        int[] octal = new int[100];
+        int i = 0;
+        while (number > 0) {
+            int rem = number % 8;
+            octal[i++] = rem;
+            number /= 8;
+        }
+
+        for (int j = i-1; j >=0 ; j--) {
+            System.out.print(octal[j]+"");
+        }
+    }
+}
